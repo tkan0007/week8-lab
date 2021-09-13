34991b48b4a4cceaa2074cbd67fcdc56aabf483b
tkan0007 <84836099+tkan0007@users.noreply.github.com>
2021-09-13 21:55:58 +0900

    Complete all task and some decorations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { isEmptyExpression } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,12 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data: any[] = [];
-  item = "";
-  newItem() {
-    this.data.push(this.item);
+  doctorDB: any[] = [];
+  firstName = '';
+  lastName = '';
+  dob = '';
+  suburb = '';
+  state = '';
+  postcode: number = 0;
+  numPatient: number = -1 ;
+  countZero = 0;
+
+  saveDoctor(){
+
+    let newDoctor={
+      firstName: this.firstName,
+      lastName: this.lastName,
+      dob: this.dob,
+      suburb: this.suburb,
+      state: this.state,
+      postcode: this.postcode,
+      numPatient: this.numPatient
+    }
+
+    this.doctorDB.push(newDoctor);
+
+    //checkZero Doctor
+    this.countZero = this.countZeroPatient();
   }
-  clearItems() {
-    this.data = [];
+
+  clearNoPatient() {
+    for(let i = 0; i<this.doctorDB.length;i++){
+      if(this.doctorDB[i].numPatient == 0){
+        this.doctorDB.splice(i,1);
+      }
+    }
+    // initialize the count
+    this.countZero = 0;
   }
+
+  clearDoctor(index:number){
+    this.doctorDB.splice(index,1);
+  }
+
+  countZeroPatient(){
+    let count = 0;
+    for(let i = 0; i<this.doctorDB.length;i++){
+      if(this.doctorDB[i].numPatient == 0){
+        count++;
+      }
+    }
+    return count;
+  }
+
 }
